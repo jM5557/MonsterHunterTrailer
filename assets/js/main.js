b1eb1db09e8d49f2f2c733fbac2bc09ad65c1083
jM5557 <javierminchala@gmail.com>
2017-03-14 09:41:40 +0900

    Content update with several trailers per game
    And some image compression as well.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,79 +2,150 @@ var games_list = {
 	"games": [{
 		name: "Monster Hunter",
 		backgrounds: "monster_hunter.jpg",
-		trailer_url: "https://www.youtube.com/embed/hl0XBLBp3Hc",
+		"videos": [
+			{ video_id: "eMONaL_QgUk"},
+			{ video_id: "6-JrgPgYcqk" },
+			{ video_id: "M_EDM4mIlao" },
+			{ video_id: "WyQAfNgX0Nw" }
+		],
 		logo: "mh_logo.png"
 	},
 	{
 		name: "Monster Hunter G",
 		backgrounds: "monster_hunter_g.jpg",
-		trailer_url: "https://www.youtube.com/embed/R_yA24waURM",
+		"videos": [
+			{ video_id: "uKRS94GWVCs" },
+			{ video_id: "_5IgI7FbVyI"},
+			{ video_id: "PWbjfqWietQ" }
+		],
 		logo: "mh_g_logo.png"
 	},
 	{
 		name: "Monster Hunter Freedom",
 		backgrounds: "monster_hunter_portable.jpg",
-		trailer_url: "https://www.youtube.com/embed/5ubaHFS-fJU",
+		"videos": [
+			{ video_id: "FN73r5G_pbQ" },
+			{ video_id: "5ubaHFS-fJU" },
+			{ video_id: "UELgBikee7M" },
+			{ video_id: "cACee9hGvvs" }
+		],
 		logo: "mh_p_logo.png"
 	},
 	{
 		name: "Monster Hunter 2 (~dos)",
 		backgrounds: "monster_hunter_2.jpg",
-		trailer_url: "https://www.youtube.com/embed/TOnUr8Ad6lc",
+		"videos": [
+			{ video_id: "EZnY6FP5Gmc" },
+			{ video_id: "TOnUr8Ad6lc" },
+			{ video_id: "gloXqXOIIIA" }
+		],
 		logo: "mh_2_logo.png"
 	},
 	{
 		name: "Monster Hunter Freedom 2",
 		backgrounds: "monster_hunter_portable_2nd.jpg",
-		trailer_url: "https://www.youtube.com/embed/lLVEOC3119c",
+		"videos": [
+			{ video_id: "K395YgQKs0A" },
+			{ video_id: "rq1LCjOuo5o" },
+			{ video_id: "lLVEOC3119c" },
+			{ video_id: "mBQ8PMNzFpc" }
+		],
 		logo: "mh_p_2nd_logo.png"
 	},
 	{
 		name: "Monster Hunter Freedom Unite",
 		backgrounds: "monster_hunter_portable_2nd_g.jpg",
-		trailer_url: "https://www.youtube.com/embed/0JoCStLsRq8",
+		"videos": [
+			{ video_id: "f_9DDfRiEhk" },
+			{ video_id: "0JoCStLsRq8" },
+			{ video_id: "ZNVdMR8AxgA" }
+		],
 		logo: "mh_p_2nd_g_logo.png"
 	},
 	{
 		name: "Monster Hunter 3 (~tri)",
 		backgrounds: "monster_hunter_3.jpg",
-		trailer_url: "https://www.youtube.com/embed/wcCsbgkfEd8",
+		"videos": [
+			{ video_id: "GQOuBJQ6_bM" },
+			{ video_id: "wcCsbgkfEd8"},
+			{ video_id: "zw_Hqg0W_fE" },
+			{ video_id: "-0Znqf9hQKs" }
+		],
 		logo: "mh_3_logo.png"
 	},
 	{
 		name: "Monster Hunter Portable 3rd",
 		backgrounds: "monster_hunter_portable_3rd.jpg",
-		trailer_url: "https://www.youtube.com/embed/X0w1iPWyoRA",
+		"videos": [
+			{ video_id: "wsGVYFBxY88" },
+			{ video_id: "98mSNK2UKqg" },
+			{ video_id: "X0w1iPWyoRA"},
+			{ video_id: "Be6LlleiV7c" },
+			{ video_id: "47DKoSDeOUQ" },
+		],
 		logo: "mh_p_3rd_logo.png"
 	},
 	{
 		name: "Monster Hunter 3 Ultimate",
 		backgrounds: "monster_hunter_3_g.jpg",
-		trailer_url: "https://www.youtube.com/embed/9wkQGEpRgL0",
+		"videos": [
+			{ video_id: "J9tQ5jbVJqo" },
+			{ video_id: "8C8T1JWDJFg" },
+			{ video_id: "9wkQGEpRgL0"},
+			{ video_id: "7uzafxEHbG0" }
+		],
 		logo: "mh_3_g_logo.png"
 	},
 	{
 		name: "Monster Hunter 4",
 		backgrounds: "monster_hunter_4.jpg",
-		trailer_url: "https://www.youtube.com/embed/XInD7Hz47MI",
+		"videos": [
+			{ video_id: "9q0OosCOxrw" },
+			{ video_id: "_5G6FcC4Nns" },
+			{ video_id: "7h3CsdHzVjY" },
+			{ video_id: "05GVsk_smO4" },
+			{ video_id: "z-NaW2WGHdY" },
+			{ video_id: "XInD7Hz47MI"},
+			{ video_id: "r9de7mRhXWk"}
+		],
 		logo: "mh_4_logo.png"
 	},
 	{
 		name: "Monster Hunter 4 Ultimate",
 		backgrounds: "monster_hunter_4_g.jpg",
-		trailer_url: "https://www.youtube.com/embed/prcQT5om1dA",
+		"videos": [
+			{ video_id: "QxFcG3Zsdm8" },
+			{ video_id: "koI16NrnRcc" },
+			{ video_id: "cNojl-pqZtk" },
+			{ video_id: "63duoCYwTaA" },
+			{ video_id: "prcQT5om1dA"},
+			{ video_id: "-crDEcdbEKY" },
+		],
 		logo: "mh_4_g_logo.png"
 	},
 	{
 		name: "Monster Hunter Generations",
 		backgrounds: "monster_hunter_x.jpg",
-		trailer_url: "https://www.youtube.com/embed/r_8-uOwDUpY",
+		"videos": [
+			{ video_id: "gZxzjDEo2WQ" },
+			{ video_id: "s0BbplGgkCY" },
+			{ video_id: "8ZABhVmzhX0" },
+			{ video_id: "r_8-uOwDUpY" },
+			{ video_id: "qmVDd6JmlUM" },
+		],
 		logo: "mh_x_logo.png"
 	},
 	{
 		name: "Monster Hunter XX",
 		backgrounds: "monster_hunter_xx.jpg",
-		trailer_url: "https://www.youtube.com/embed/MnpNGzNoOY0",
+		"videos": [
+			{ video_id: "5VV5-opm1TE" },
+			{ video_id: "XiOBszkWCp0" },
+			{ video_id: "MnpNGzNoOY0" },
+			{ video_id: "dtxYlOPAutE" },
+			{ video_id: "30ikOWOgUvY" },
+
+		],
 		logo: "mh_xx_logo.png",
 	}],
 
--- assets/js/templates/templates.js
+++ assets/js/templates/templates.js
@@ -1,14 +1,15 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['games'] = template({"1":function(container,depth0,helpers,partials,data) {
-    var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
-
-  return ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.even_pos : depth0),{"name":"if","hash":{},"fn":container.program(2, data, 0),"inverse":container.program(4, data, 0),"data":data})) != null ? stack1 : "")
+    var stack1, helper, options, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", buffer = 
+  ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.even_pos : depth0),{"name":"if","hash":{},"fn":container.program(2, data, 0),"inverse":container.program(4, data, 0),"data":data})) != null ? stack1 : "")
     + "			<h1>"
-    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
-    + "</h1>\r\n			<iframe src=\""
-    + alias4(((helper = (helper = helpers.trailer_url || (depth0 != null ? depth0.trailer_url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"trailer_url","hash":{},"data":data}) : helper)))
-    + "\" frameborder=\"0\" allowfullscreen></iframe>\r\n		</div>\r\n";
+    + container.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
+    + "</h1>\r\n			\r\n			<div class = \"video_container\">\r\n";
+  stack1 = ((helper = (helper = helpers.videos || (depth0 != null ? depth0.videos : depth0)) != null ? helper : alias2),(options={"name":"videos","hash":{},"fn":container.program(6, data, 0),"inverse":container.noop,"data":data}),(typeof helper === alias3 ? helper.call(alias1,options) : helper));
+  if (!helpers.videos) { stack1 = helpers.blockHelperMissing.call(depth0,stack1,options)}
+  if (stack1 != null) { buffer += stack1; }
+  return buffer + "			</div>\r\n		</div>\r\n";
 },"2":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
@@ -16,7 +17,7 @@ templates['games'] = template({"1":function(container,depth0,helpers,partials,da
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
     + " class = \"game\" style = \"\r\n			background: url('assets/images/backgrounds/"
     + alias4(((helper = (helper = helpers.backgrounds || (depth0 != null ? depth0.backgrounds : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"backgrounds","hash":{},"data":data}) : helper)))
-    + "') no-repeat center center fixed;\r\n			background-size: cover\">\r\n";
+    + "') no-repeat center center fixed;\r\n			background-size: cover;\">\r\n";
 },"4":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
@@ -25,6 +26,12 @@ templates['games'] = template({"1":function(container,depth0,helpers,partials,da
     + " class = \"game\" style = \"\r\n			background: url('assets/images/backgrounds/"
     + alias4(((helper = (helper = helpers.backgrounds || (depth0 != null ? depth0.backgrounds : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"backgrounds","hash":{},"data":data}) : helper)))
     + "') no-repeat center center;\r\n			background-size: cover\">\r\n";
+},"6":function(container,depth0,helpers,partials,data) {
+    var helper;
+
+  return "				<iframe src=\"https://www.youtube.com/embed/"
+    + container.escapeExpression(((helper = (helper = helpers.video_id || (depth0 != null ? depth0.video_id : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"video_id","hash":{},"data":data}) : helper)))
+    + "\" frameborder=\"0\" allowfullscreen></iframe>\r\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, options;
 
